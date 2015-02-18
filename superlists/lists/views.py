fc67afb9acc37c900acc749c9e11585d4603b4c5
AtomsForPeace <adam.p.bannister@gmail.com>
2015-02-18 21:53:29 +0100

    add form, print out of form and test post of form

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
