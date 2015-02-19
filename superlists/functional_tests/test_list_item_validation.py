2cf7edd8d3f817bbd49289e757b8ef080ffe15a6
AtomsForPeace <adam.p.bannister@gmail.com>
2015-02-19 20:03:51 +0100

    add basic item validation, check for empty list item

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -4,4 +4,21 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        self.fail('add it')
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
