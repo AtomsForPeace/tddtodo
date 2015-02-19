96f49065e8f030c4908124761275860805c6a016
AtomsForPeace <adam.p.bannister@gmail.com>
2015-02-19 21:41:33 +0100

    use new form from forms.py. breaks functional tests

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -6,19 +6,23 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    maxDiff = None
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string(
+            'home.html', {'form': ItemForm()})
+        self.assertMultiLineEqual(
+            response.content.decode(), expected_html)
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
