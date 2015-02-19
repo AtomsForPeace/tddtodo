8963e41d97cc7f868e859ca9bb1fbd768543f866
AtomsForPeace <adam.p.bannister@gmail.com>
2015-02-19 12:36:58 +0100

    new URL and view for adding to exiting lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -65,5 +65,3 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-        self.fail('Finish the test!')
